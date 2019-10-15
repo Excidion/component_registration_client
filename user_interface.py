c4870ce3662ef5930c9082dea495d874aecb97eb
Cedric <cedric.wilting@stud.uni-due.de>
2019-10-15 15:47:17 +0200

    Display

--- user_interface.py
+++ user_interface.py
@@ -8,6 +8,8 @@ from qr_code import qr_cam
 class UserInterface():
     def __init__(self, connection_manager):
         self.root = tk.Tk()
+        self.root.title("Component Registration")
+        self.root.bind("<Escape>", lambda x: self.root.destroy())
         self.connection_manager = connection_manager
         self.online_status_indicator = tk.Button(
             self.root,
@@ -19,20 +21,49 @@ class UserInterface():
         self.new_part_button = tk.Button(
             self.main_frame,
             text = "New Component",
+            width = 30,
+            height = 5,
         )
         self.scan_part_button = tk.Button(
             self.main_frame,
             text = "Scan Component",
+            width = 30,
+            height = 5,
             command = self.scan_part,
         )
 
+        self.working_frame = tk.LabelFrame(
+            self.main_frame,
+            text = "Component",
+        )
+        self.display_id = DisplayFrame(
+            self.working_frame,
+            label = "ID",
+        )
+        self.display_assembly_group = DisplayFrame(
+            self.working_frame,
+            label = "Assembly Group",
+        )
+        self.display_module = DisplayFrame(
+            self.working_frame,
+            label = "Module",
+        )
+        self.display_state = DisplayFrame(
+            self.working_frame,
+            label = "State",
+        )
 
         self.main_frame.pack()
         self.online_status_indicator.pack()
 
         self.new_part_button.grid(row=0, column=0)
         self.scan_part_button.grid(row=1, column=0)
+        self.working_frame.grid(row=0, column=1, rowspan=2)
 
+        self.display_id.grid(row=0, column=0)
+        self.display_assembly_group.grid(row=0, column=1)
+        self.display_module.grid(row=0, column=2)
+        self.display_state.grid(row=0, column=3)
 
 
     def start(self):
@@ -50,7 +81,7 @@ class UserInterface():
 
     def update_online_status(self):
         if self.connection_manager.check_online_status():
-            text = "Online"
+            text = f"Online as " + self.connection_manager.config["user"]
         else:
             text = "Offline"
         self.online_status_indicator.config(text=text)
@@ -62,6 +93,22 @@ class UserInterface():
 
     def scan_part(self):
         id = qr_cam()
+        self.display_id.set(id)
+
+
+
+class DisplayFrame(tk.LabelFrame):
+    def __init__(self, *args, label, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label = label
+        self.config(text=self.label)
+        self.display = tk.Label(self, text="")
+        self.display.pack()
+        self.set()
+
+    def set(self, entry=None):
+        display_text = entry if not entry == None else f"<{self.label}>"
+        self.display.config(text=display_text)
 
 
 
